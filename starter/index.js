517a80a7b03a99235e636c443d7f5d136895bfb2
Toni97 <ayovcheva75@gmail.com>
2024-02-11 17:06:47 +0000

    adding code for caaling manager class

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,31 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Array to store team members
+const teamMembers = [];
+
+// Function to prompt for manager's information
+function promptManager() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter the team manager's name:",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the team manager's employee ID:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the team manager's email address:",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Enter the team manager's office number:",
+    },
+  ]);
+}
